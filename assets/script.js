474160df7d89d6ee8e5ceae41ae14bfbdaa6a2f7
Daniel Carazo <danielcarazo20@gmail.com>
2021-06-24 22:05:56 -0400

    edit css, added timeblock funtions, repositioned var to global scope

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,20 @@
+// current date variable to display on once page opens
+$("#currentDay").text(moment().format("MMM Do YYYY"))
+// current time variable 
+var currentTime = moment().hour();
+// console.log(currentTime);
+
 // run page once DOM is ready for JS code to execute
-// $(function() {
-//     console.log( "ready!" );
-$(document).ready(function() {
-    // display date
-    $("#currentDay").text(moment().format("MMM Do YY"))
+$(function() {
+    // console.log('function working');;
 
     // on 'click' event listener 
     $('.saveBtn').on("click", function() {
-        var event = $(this).siblings(".description").val(); 
+        var value = $(this).siblings(".description").val(); 
         var time = $(this).parent().attr("id"); 
 
         // saves key value into the storage
-        localStorage.setItem(event, time);
+        localStorage.setItem(value, time);
     })
 
     // returns the key value of what is in the storage
@@ -24,14 +27,34 @@ $(document).ready(function() {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+    function timeBlock () {
+    
+        $('.time-block').each(function () {
+        var timeSection = parseInt($(this).attr("id").split("hour")[1]);  
+        console.log(timeSection);
+        
+        if (timeSection < currentTime) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }  else if (timeSection === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }  else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+            }
+        })
+    }
+    timeBlock();
 });
+
 
 // create a function on current time of day
 // if/else statements in regards to current time of day, blocks will be highlighted depending on time of day
 // run funcction 
-
-    var currentTime = moment().format('h:mm');
-    console.log(currentTime);
-
-    var timeBlock = []
 
